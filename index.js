4cdb4f25e9b2f7af8c1c3af47358fa0e6c3607dc
Corina <corinajimenez061102@gmail.com>
2025-12-22 13:25:24 -0300

    configuraciones y dependencias

---
+++ index.js
@@ -0,0 +1,7 @@
+import Server from './src/server/config.js';
+
+const server = new Server();
+
+//invocar al metodo lsiten
+
+server.listen();
---
+++ src/server/config.js
@@ -0,0 +1,23 @@
+import express from "express";
+import cors from "cors";
+import morgan from "morgan";
+
+export default class Server {
+  constructor() {
+    this.app = express();
+    this.port = process.env.PORT || 3000;
+    this.middlewares();
+  }
+
+  middlewares() {
+    this.app.use(cors()); //permite escuchar conexiones remotas
+    this.app.use(express.json()); //permite interpretar los datos json que llegan en una solicitud
+    this.app.use(morgan("dev")); //ofrece datos extras en el backend
+  }
+
+  listen() {
+    this.app.listen(this.port, () =>
+      console.info(`El servidor se esta ejecutando en: ${this.port}`)
+    );
+  }
+}
